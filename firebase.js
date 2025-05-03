61470199ff1eb967fb33371328f9cfef7c06c2b2
Paragsha <133567077+Paragsha@users.noreply.github.com>
2025-05-04 01:18:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCly_nrNKyo-JhcC_xl4fpxGX9s12O8GFI",
   authDomain: "novairisbazaar.firebaseapp.com",
   projectId: "novairisbazaar",
-  storageBucket: "novairisbazaar.appspot.com",
+  storageBucket: "novairisbazaar.firebasestorage.app",
   messagingSenderId: "460256816663",
   appId: "1:460256816663:web:bedaff3a8a64c07b75b652"
 };
